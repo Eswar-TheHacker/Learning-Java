b946dcbb6aea8892078d4a79adfd17674ef3725b
eswarlella <eswar.lella1995@gmail.com>
2026-03-16 20:13:22 +0530

    Operators

---
+++ src/ex_04_Operators/Lab040_Relational_Operators.java
@@ -0,0 +1,24 @@
+package ex_04_Operators;
+
+public class Lab040_Relational_Operators {
+    public static void main(String[] args) {
+        // < Less Than
+        // < =  -> Less than or equal to
+        // > Greater
+        // > = Greater or equal
+        // == ->  Equal to (but checking)
+        // != -> Not equal
+
+        int a = 10;
+        int b = 30;
+        boolean c = a > b;
+        System.out.println(c);
+
+        int age_mamitha = 33;
+        int age_eswar = 32;
+
+        boolean result = age_eswar >= age_mamitha;
+        System.out.println(result);
+
+    }
+}
---
+++ src/ex_04_Operators/Lab042_interview_Concat_plus.java
@@ -0,0 +1,23 @@
+package ex_04_Operators;
+
+public class Lab042_interview_Concat_plus {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+
+        //+ -> String -> Concatenation
+
+        String first_name = "Eswar";
+        String last_name = "lella";
+        System.out.println(first_name+last_name);
+
+             //System.out.println(first_name-last_name);
+//        System.out.println(first_name*last_name);
+//        System.out.println(first_name/last_name);
+
+        // + - behave arch int
+        // + - concat String
+
+    }
+}
